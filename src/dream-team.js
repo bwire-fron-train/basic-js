b298223ff075b044aa86eb75bf01915717d361c3
BarbedWire <e.barbedwire@gmail.com>
2020-02-25 10:52:36 +0300

    feat: dream-team solved

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,10 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (Array.isArray(members)) {
+    return members.filter(name => typeof name === 'string' && name.length !== 0)
+      .map(name => name.trim()[0].toUpperCase())
+      .sort()
+      .join('');
+  } else {
+    return false;
+  }
 };
