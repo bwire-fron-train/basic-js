6c143ef6de65eb1e13e3eaf81737c212bc6dc451
BarbedWire <e.barbedwire@gmail.com>
2020-02-29 23:31:32 +0300

    feat: solve transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,33 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function transform(arr) {
+    if (Array.isArray(arr)) {
+        if (arr == []) {
+            return [];
+        } else {
+            const acc = [];
+            for (let i = 0; i < arr.length; i++) {
+                if (arr[i] === '--discard-next') {
+                    if (arr[i + 1] != undefined) {
+                        i += 1;
+                    }
+                } else if (arr[i] === '--discard-prev') {
+                    acc.pop(); 
+                } else if (arr[i] === '--double-next') {
+                    if (arr[i + 1] != undefined) {
+                        acc.push(arr[i + 1]);
+                    }
+                } else if (arr[i] === '--double-prev') { 
+                    if (arr[i - 1] != undefined) {
+                        acc.push(arr[i -1]);
+                    } 
+                } else {
+                    acc.push(arr[i]);
+                }
+            }
+            return acc;
+        }
+    } else {
+        throw new Error;
+    }  
 };
+
+//console.log(transform( [ '--double-prev', 22, '--discard-next', 'DEF', 22, '--double-next' ] ));
