4c22b162372521822a2414d9d809f5c849d33a2b
BarbedWire <e.barbedwire@gmail.com>
2020-03-04 07:28:54 +0300

    feat: solve extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,34 @@
-module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function repeater(str, options, sep) {
+  function getRepeated(s, times, sep) {
+    if (s === undefined) {
+      return '';
+    }
+    if (s === null) {
+      s = 'null';
+    }
+    if (typeof s != 'string') {
+       s = s.toString();
+    }
+    if (!times || times == 0) {
+      return s;
+    } 
+
+    const a = Array(times);
+    a.fill(s);
+    return a.join(sep);
+  }
+
+  if (!options.separator || options.separator == '') {
+    options.separator = '+';
+  }
+  if (!options.additionSeparator || options.additionSeparator == '') {
+    options.additionSeparator = '|';
+  }
+  
+  return getRepeated(
+    str + getRepeated(options.addition, options.additionRepeatTimes, options.additionSeparator), 
+    options.repeatTimes, 
+    options.separator
+  );
 };
   
